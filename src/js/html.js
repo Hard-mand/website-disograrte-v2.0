e817c1bec6b3059f9bbf7df56cc9b0f4a31bc707
hardmand <javt28@outlook.com>
2023-09-04 19:51:21 -0500

    full mobile and section2

--- src/js/html.js
+++ src/js/html.js
@@ -17,6 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // index.html
   html("../html/home/head.html", "head");
   html("../html/home/section1.html", "section1");
-  // html("../html/home/section2.html", "section2");
+  html("../html/home/section2.html", "section2");
   // html("../html/home/section3.html", "section3");
 })
--- src/js/theme.js
+++ src/js/theme.js
@@ -3,7 +3,13 @@ const dark = () => {
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
   document.querySelector("#body").setAttribute("class", "container-md bg-black");
   document.querySelector("h1").setAttribute("class", "text-white fw-bold m-5 m-md-0 mb-3 mb-md-4");
-  document.querySelector("#p-header").setAttribute("class", "text-white fs-5 mx-5 mx-md-0");
+  document.querySelector("#p-header").setAttribute("class", "text-body fs-5 mx-5 mx-md-0");
+
+  var cards = document.querySelectorAll(".card");
+  cards.forEach(function (card) {
+    card.setAttribute("class", "card mb-3 bg-secondary-subtle");
+  });
+
   setCookie("theme", "dark");
   document.body.style.visibility = "visible"; // Mostrar el cuerpo
 };
@@ -13,7 +19,13 @@ const light = () => {
   document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
   document.querySelector("#body").setAttribute("class", "container-md bg-light");
   document.querySelector("h1").setAttribute("class", "text-black fw-bold m-5 m-md-0 mb-3 mb-md-4");
-  document.querySelector("#p-header").setAttribute("class", "text-black fs-5 mx-5 mx-md-0");
+  document.querySelector("#p-header").setAttribute("class", "text-body fs-5 mx-5 mx-md-0");
+
+  var cards = document.querySelectorAll(".card");
+  cards.forEach(function (card) {
+    card.setAttribute("class", "card mb-3 bg-light");
+  });
+
   setCookie("theme", "light");
   document.body.style.visibility = "visible"; // Mostrar el cuerpo
 };
